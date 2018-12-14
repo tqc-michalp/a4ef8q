3749d674b7fcc731dc26a9ab1e4047dab3dd9167
tqc-michalp <michal.pawlak@thinqcraft.com>
2018-12-14 15:14:16 +0100

    finished CategoryAttribute implementation

--- app/controllers/category_attributes_controller.rb
+++ app/controllers/category_attributes_controller.rb
@@ -1,4 +1,50 @@
 # frozen_string_literal: true
 
 class CategoryAttributesController < ApplicationController
+  before_action :set_category_attribute, only: %i[edit update destroy]
+
+  def index
+    @category_attributes = CategoryAttribute.grouped_by_category
+  end
+
+  def new
+    @category_attribute = CategoryAttribute.new
+  end
+
+  def edit; end
+
+  def create
+    @category_attribute = CategoryAttribute.new(category_attribute_params)
+
+    if @category_attribute.save
+      redirect_to category_attributes_url, flash: { success: 'Atrybut kategorii został utworzony' }
+    else
+      render :new, flash: { error: 'Atrybut kategorii nie został utworzony' }
+    end
+  end
+
+  def update
+    if @category_attribute.update(category_attribute_params)
+      redirect_to category_attributes_url, flash: { success: 'Atrybut kategorii został zaktualizowany' }
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category_attribute.destroy
+    redirect_to category_attributes_url, flash: { info: 'Atrybut kategorii zosta usunięty' }
+  end
+
+  private
+
+  def set_category_attribute
+    @category_attribute = CategoryAttribute.find(params[:id])
+    @category_attribute.name_type.symbolize_keys!
+    @category_attribute
+  end
+
+  def category_attribute_params
+    params.require(:category_attribute).permit(:category_id, name_type: %i[name type])
+  end
 end
--- app/models/category_attribute.rb
+++ app/models/category_attribute.rb
@@ -2,4 +2,9 @@
 
 class CategoryAttribute < ApplicationRecord
   belongs_to :category
+  store_accessor :name_type
+
+  enum type: %i[string integer object array boolean null]
+
+  scope :grouped_by_category, -> { group(:category_id, :id) }
 end
